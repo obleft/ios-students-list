baf7809a2ce31a6c1631138213a0339ae5f1b0dc
Ben Hakes <bhhakes@gmail.com>
2018-12-03 13:25:01 -0600

    implement sort feature by f/l name

--- Students/Model/Student.swift
+++ Students/Model/Student.swift
@@ -10,4 +10,13 @@ import Foundation
 
 struct Student: Codable {
     var name: String
+    
+    var firstName: String {
+        return String(name.split(separator: " ")[0])
+    }
+    
+    var lastName: String? {
+        guard let last = name.split(separator: " ").dropFirst().last else {return nil}
+        return String(last)
+    }
 }
--- Students/View Controllers/MainViewController.swift
+++ Students/View Controllers/MainViewController.swift
@@ -10,21 +10,60 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // MARK: - Properties
+    private let networkClient = NetworkClient()
+    private var studentsTableViewController: StudentsTableViewController!
+    
+    private var students: [Student] = []{
+        didSet {
+            updateSort()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        networkClient.fetchStudents { (students, error) -> Void in
+            if let error = error {
+                NSLog("Error getting students: \(error)")
+                return
+            }
+            
+            self.students = students ?? []
+        }
     }
     
-
-    /*
+    @IBOutlet weak var sortSelector: UISegmentedControl!
+    @IBAction func sort(_ sender: Any) {
+        updateSort()
+    }
+    
+    
+    private func updateSort(){
+        let sortedStudents: [Student]
+        if sortSelector.selectedSegmentIndex == 0 {
+            // Sort by first name
+            sortedStudents = students.sorted{ $0.firstName < $1.firstName}
+        } else {
+            sortedStudents = students.sorted {($0.lastName ?? "") < ($1.lastName ?? "")
+            }
+        
+        }
+        studentsTableViewController.students = sortedStudents
+    }
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "embedStudentsTableView"{
+            let studentsTableVC = segue.destination as! StudentsTableViewController
+            studentsTableViewController = studentsTableVC
+        }
     }
-    */
+    
 
+    
+    
 }
